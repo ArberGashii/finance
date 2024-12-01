75cdc8267447c45d1205a297c9c13ddbbb97230b
ArberGashii <ArberGashii@users.noreply.github.com>
2024-12-01 11:45:44 +0000

    deploy: 63a01cdecff087ce79465d3f0f9d099605433bef

---
+++ assets/AddShipments-CWNj0egR.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DQwrSmHc.js";const d=()=>t.jsx("div",{children:"AddShipments"});export{d as default};
---
+++ assets/Dashboard-Oz9khHid.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DQwrSmHc.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
